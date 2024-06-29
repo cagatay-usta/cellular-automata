a2032c2e2be341b489231a75ff374c385b7b09a2
cagatay-usta <cagatay_usta@windowslive.com>
2024-06-29 21:16:48 +0300

    add basic grid container and grid initialization

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-display',
@@ -7,4 +7,23 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   templateUrl: './display.component.html',
   styleUrl: './display.component.scss',
 })
-export class DisplayComponent {}
+export class DisplayComponent {
+  rows = signal(100);
+  cols = signal(50);
+  grids = computed<string[][]>(() =>
+    this.initializeGridArray(this.rows(), this.cols(), 'alive')
+  );
+
+  constructor() {}
+
+  // TODO: create a model for grid class, grids should track neighbors and compute own states themselves
+  private initializeGridArray(
+    rows: number,
+    cols: number,
+    state: string = 'dead'
+  ): string[][] {
+    return Array.from({ length: rows }, () =>
+      Array.from({ length: cols }, () => state)
+    );
+  }
+}
